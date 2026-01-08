e8cca4083b7147184a474938aa011c0aac72d5e1
Rigoberto Hernandez <142679865+RigobertoEHA1@users.noreply.github.com>
2026-01-08 02:09:38 -0600

    gof

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
                 <h1>Not Found</h1>
                 <p>The requested URL was not found on this server.</p>
                 <hr />
-                <address>Apache/2.4.41 (Ubuntu) Server at normalessephgobmx.vercel.app Port 443</address>
+                <address>Apache/2.4.41 (Ubuntu) Server at normales-seph-gob-mx.vercel.app Port 443</address>
             </body>
         </html>
     );
